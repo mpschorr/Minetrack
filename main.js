e9d3c7e1f499d379807d92b23160bdd9b19502a1
mpschorr <real.jeel@gmail.com>
2024-02-15 17:43:20 -0500

    fork with genres & minehut servers

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 const App = require('./lib/app')
 const ServerRegistration = require('./lib/servers')
 
+
 const logger = require('./lib/logger')
 
 const config = require('./config')
 const servers = require('./servers')
 
 const app = new App()
+
+const { hsvToRgb } = require('./lib/color')
+
+const genreHues = {
+  Lifesteal: 340 / 360,
+  Box: 220 / 360,
+  'Other Eco': 175 / 360,
+  Gens: 135 / 360,
+  Kits: 30 / 360,
+}
 
 servers.forEach((server, serverId) => {
   // Assign a generated color for each servers.json entry if not manually defined
@@ -17,8 +28,16 @@ servers.forEach((server, serverId) => {
       hash = server.name.charCodeAt(i) + ((hash << 5) - hash)
     }
 
-    const color = Math.floor(Math.abs((Math.sin(hash) * 10000) % 1 * 16777216)).toString(16)
-    server.color = '#' + Array(6 - color.length + 1).join('0') + color
+    const h = genreHues[server.genre];
+    const s = 0.15 * Math.sin(hash) + 0.85;
+    const v = 0.3 * Math.cos(hash) + 0.7;
+    const colorObj = hsvToRgb(h,s,v);
+    const color = ((colorObj.r << 16) + (colorObj.g << 8) + (colorObj.b)).toString(16);
+    console.log(colorObj, color)
+    server.color = `#${Array(6 - color.length + 1).join('0')}${color}`
+
+    // const color = Math.floor(Math.abs((Math.sin(hash) * 10000) % 1 * 16777216)).toString(16)
+    // server.color = '#' + Array(6 - color.length + 1).join('0') + color
   }
 
   // Init a ServerRegistration instance of each entry in servers.json
